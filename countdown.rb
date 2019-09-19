9850b36bbcb3fb9ebe32635c869175a376bbeeb1
cedric e goddard <8ceg6@comcast.net>
2019-09-19 15:14:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(number)
-  while number >
-   countdown_with_sleep.sleep(5)
+  while number > 0
+   countdown_with_sleep.sleep 5
     puts "#{number} SECOND(S)!"
     number -= 1
   end
